5c31923c9bc3a2c1a4a3d821ac6d16835c99d9d5
Justin Gordon <linuxfreak1030@gmail.com>
2025-03-10 19:36:17 -0400

    complete assignment 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToManipulate,...valuesToRemove) {
+    for (const value of valuesToRemove) {
+        let keepGoing = true;
+        while (keepGoing) {
+            const indexToRemove = arrayToManipulate.indexOf(value);
+            if (indexToRemove != -1) {
+                arrayToManipulate.splice(indexToRemove,1)
+            } else keepGoing = false;
+        }
+    }
+    return arrayToManipulate;
 };
 
 // Do not edit below this line
