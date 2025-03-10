0353212cf80dc5126dbea05dc37e9b859b0fbaae
Justin Gordon <linuxfreak1030@gmail.com>
2025-03-10 10:30:04 -0400

    complete assignment 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let reversedString = "";
+    const stringSplitToChars = stringToReverse.split('');
+    const stringArrayLength = stringSplitToChars.length
+    for(i=0;i<stringArrayLength;i++) {
+        reversedString += stringSplitToChars.pop();
+    }
+    return reversedString;
 };
 
+console.log(reverseString("Hello"));
 // Do not edit below this line
 module.exports = reverseString;
