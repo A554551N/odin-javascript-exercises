c2fa57dc3279ce670b1c7c1f5f8008dbdd0f44c7
Justin Gordon <linuxfreak1030@gmail.com>
2025-03-10 20:36:27 -0400

    Complete assignment 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+  // C = (F − 32) × 5/9
+  const degreesC = (degreesF - 32) * (5/9);
+  const degreesCRounded = Math.round(degreesC * 10)/10;
+  return degreesCRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesC) {
+  // F = (C × 9/5) + 32
+  const degreesF = (degreesC * (9/5)) + 32;
+  const degreesFRounded = Math.round(degreesF * 10)/10; 
+  return degreesFRounded;
 };
 
 // Do not edit below this line
