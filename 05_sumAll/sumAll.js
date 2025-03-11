f4bccae4f47efb39b33b8bb0ff66c3156f3091a5
Justin Gordon <linuxfreak1030@gmail.com>
2025-03-10 20:10:38 -0400

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber,secondNumber) {
+    if (firstNumber < 0 || secondNumber < 0) {
+        return 'ERROR'
+    } else if (!Number.isInteger(firstNumber)||!Number.isInteger(secondNumber)) {
+        return 'ERROR'
+    }
+    
+    const smallerNumber = Math.min(firstNumber,secondNumber);
+    const largerNumber = Math.max(firstNumber,secondNumber);
+    let sum = 0;
+    for (i=smallerNumber;i<=largerNumber;i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
