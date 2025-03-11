4de671bfae0f22fda658a10815808654b762b035
Justin Gordon <linuxfreak1030@gmail.com>
2025-03-10 20:24:23 -0400

    complete assignment 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //if divisible by four AND not divisible by 100 AND divisible by 400
+    const remainderOfYearDividedBy4 = year % 4;
+    const remainderOfYearDividedBy100 = year % 100;
+    const remainderOfYearDividedBy400 = year % 400;
 
+    if (remainderOfYearDividedBy4 == 0) {
+        if (remainderOfYearDividedBy100 != 0 || remainderOfYearDividedBy400 == 0) return true
+    }
+
+    return false
 };
 
 // Do not edit below this line
