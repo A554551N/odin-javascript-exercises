37cc3ca71a78c3d050ae33cdf39d7b6505f9dbe3
Justin Gordon <linuxfreak1030@gmail.com>
2025-03-14 19:57:07 -0400

    complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (stringToTest) {
+    const azRegex = /[a-z0-9]/
+    //split the string after putting it in lower case
+    const splitString = stringToTest.toLowerCase().split("").filter((char) => azRegex.test(char))
+    //filter off punctuation and spaces
+    const splitWithoutPunctuation = splitString.filter((char) => azRegex.test(char))
+    
+    // deep copy the array.  What follows exists because I didn't know .reverse() was a thing
+    const arrayToReverse = [...splitWithoutPunctuation];
+    const reversedArray = new Array();
+    // get the length before you tear the array up
+    const reversedArrayLength = arrayToReverse.length;
+    for (let i=0; i<reversedArrayLength;i++) {
+        //remove the last item of the array and put it on the end of a new array
+        reversedArray[i] = arrayToReverse.pop()
+    }
+    if (splitWithoutPunctuation.join("") === reversedArray.join("")) return true;
+    return false;
 };
 
 // Do not edit below this line
