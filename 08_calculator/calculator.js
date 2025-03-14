6d19932aa3bbbae82379e7b3c4ad3c4b9183d3bc
Justin Gordon <linuxfreak1030@gmail.com>
2025-03-14 19:34:04 -0400

    complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b;
+};
+
+const subtract = function(a,b) {
+	return a - b;
+};
+
+const sum = function(arr) {
+	return arr.reduce((sumOfNum,num) => sumOfNum + num,0)
+};
+
+const multiply = function(arr) {
+  return arr.reduce((total,currNum) => total * currNum)
+};
+
+const power = function(a,b) {
+  return Math.pow(a,b);
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(factor) {
+  if (factor === 0) return 1;
+	let total = factor;
+  let runningFactor = factor - 1;
+  for(runningFactor;runningFactor>1;runningFactor--)
+    total = total * runningFactor;
+  return total;
 };
 
 // Do not edit below this line
