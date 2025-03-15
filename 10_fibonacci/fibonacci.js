6f6e7445ea8f67db61aed2dc53d2614289c1a26b
Justin Gordon <linuxfreak1030@gmail.com>
2025-03-15 10:34:16 -0400

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibNum) {
+// initialize array to hold fibonacci sequence, start with 0 to prime for first run
+const numTimesToRun = parseInt(fibNum);
+if (numTimesToRun === 0) return 0;
+if (numTimesToRun < 0) return "OOPS";
+const fib = [0,1]
+    for(let i=1;i<numTimesToRun;i++){
+        fib.push(fib[i]+fib[i-1]);
+        console.table(fib);
+    }
+    return fib[fib.length - 1];
 };
 
 // Do not edit below this line
