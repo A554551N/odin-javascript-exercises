576bf5a60005a14eba57b44bc8bce6e0ae232833
Justin Gordon <linuxfreak1030@gmail.com>
2025-03-15 10:51:11 -0400

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrayOfPeople) {
+    arrayOfPeople.sort((a,b) => {
+        console.log(`A Date of Death: ${a.yearOfDeath}`)
+        console.log(`B Date of Death: ${b.yearOfDeath??2022}`)
+        ageOfA = ((a.yearOfDeath ?? 2025) - a.yearOfBirth);
+        ageOfB = ((b.yearOfDeath ?? 2025) - b.yearOfBirth);
+        return ageOfA > ageOfB ? 1 : -1;
+    })
+    return arrayOfPeople.pop();
 };
 
+findTheOldest([{yearofBirth:1500},{yearOfDeath:1800,yearofBirth:1400}])
 // Do not edit below this line
 module.exports = findTheOldest;
